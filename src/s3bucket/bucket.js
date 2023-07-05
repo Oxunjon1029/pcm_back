aa69ff3b36eea92a6649c2ca9d84a7ddcca97746
Oxunjon1029 <abdusalomov2019oxunjon@gmail.com>
2023-07-05 20:35:52 +0500

    little alteration

--- src/s3bucket/bucket.js
+++ src/s3bucket/bucket.js
@@ -1,38 +1,33 @@
-require('dotenv').config();
-const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
-const { randomBytes } = require('crypto');
-const { promisify } = require('util');
+require('dotenv').config()
+const aws = require('aws-sdk')
+const { promisify } = require('util')
+const crypto = require('crypto');
+const region = "eu-north-1"
+const bucketName = "personalcollectionmanagement"
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY
+const randomBytes = promisify(crypto.randomBytes)
 
-const region = "eu-north-1";
-const bucketName = "personalcollectionmanagement";
-const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
-const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
 
-const randomBytesAsync = promisify(randomBytes);
-
-const s3Client = new S3Client({
+const s3 = new aws.S3({
   region,
-  credentials: {
-    accessKeyId,
-    secretAccessKey
-  }
-});
+  accessKeyId,
+  secretAccessKey,
+  signatureVersion: 'v4',
+})
 
 const generateUploadUrl = async () => {
-  const rawBytes = await randomBytesAsync(16);
-  const imageName = rawBytes.toString('hex');
+  const rawBytes = await randomBytes(16)
+  const imageName = rawBytes.toString('hex')
 
-  const params = {
+  const params = ({
     Bucket: bucketName,
     Key: imageName,
-  };
-
-  const command = new PutObjectCommand(params);
-  const uploadUrl = await s3Client.getSignedUrl(command);
-
-  return uploadUrl;
-};
+  })
+  const uploadUrl = await s3.getSignedUrlPromise('putObject', params)
+  return uploadUrl
+}
 
 module.exports = {
   generateUploadUrl
-};
+}
