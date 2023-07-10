9d1148999d5c0b1af5eab41229720d6ca4c7f753
Oxunjon1029 <abdusalomov2019oxunjon@gmail.com>
2023-07-10 22:16:35 +0500

    some changes

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,7 +1,7 @@
 const { StatusCodes } = require('http-status-codes');
 
 module.exports.isAuthenticated = (req, res, next) => {
-  console.log(req.user)
+  console.log(req.session,req)
   if (req.session.user) {
     next()
   } else {
