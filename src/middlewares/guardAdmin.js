033fa85747053581c44b47524230d09cc0af4c11
Oxunjon1029 <abdusalomov2019oxunjon@gmail.com>
2023-07-11 17:03:58 +0500

    added passport

--- src/middlewares/guardAdmin.js
+++ src/middlewares/guardAdmin.js
@@ -1,6 +1,7 @@
 const { StatusCodes } = require('http-status-codes');
 
 module.exports.isAuthenticatedAndAdmin = async (req, res, next) => {
+  console.log(req.user)
   if (req.user.role !== 'admin') {
     return res.status(StatusCodes.FORBIDDEN).json({ message: 'Access denied. User is not an admin.' });
   }
