5f7ff2fe51f33964429e9724b7d5c5df88f4e42d
Oxunjon1029 <abdusalomov2019oxunjon@gmail.com>
2023-07-11 16:54:32 +0500

    added passport

--- src/middlewares/guardAdmin.js
+++ src/middlewares/guardAdmin.js
@@ -2,6 +2,7 @@ const { StatusCodes } = require('http-status-codes');
 const User = require('../models/users')
 
 module.exports.isAuthenticatedAndAdmin = async (req, res, next) => {
+  console.log(req.body)
   const user = await User.findOne({ email: req.body.email })
   if (user && user.role === 'admin') {
     next()
