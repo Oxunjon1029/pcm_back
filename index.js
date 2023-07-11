69140209b6d04ffcd12830f0e64e9082bcb92888
Oxunjon1029 <abdusalomov2019oxunjon@gmail.com>
2023-07-11 14:06:25 +0500

    added passport

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ app.use(require('cookie-session')({
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use('/api/v1', authRouter)
 app.use('/api/v1', userRouter)
 app.use('/api/v1', collectionRouter)
