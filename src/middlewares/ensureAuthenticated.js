6066bdfa1cfe15bc3fe4fd06c1ce026097108559
Oxunjon1029 <abdusalomov2019oxunjon@gmail.com>
2023-07-11 16:36:01 +0500

    added passport

--- src/middlewares/ensureAuthenticated.js
+++ src/middlewares/ensureAuthenticated.js
@@ -1,18 +1,19 @@
 const jwt = require('jsonwebtoken');
 const { StatusCodes } = require('http-status-codes')
 module.exports.ensureAuthenticated = (req, res, next) => {
-  console.log(req.headers)
-  // if (!token) {
-  //   return res.status(StatusCodes.UNAUTHORIZED).json({ message: 'No token provided' });
-  // }
+  const token = req.headers.authorization;
 
-  // // Verify the token
-  // jwt.verify(token, process.env.JWT_SECRET, (err) => {
-  //   if (err) {
-  //     return res.status(StatusCodes.UNAUTHORIZED).json({ message: 'Invalid token' });
-  //   }
+  if (!token) {
+    return res.status(StatusCodes.UNAUTHORIZED).json({ message: 'No token provided' });
+  }
+
+  // Verify the token
+  jwt.verify(token, process.env.JWT_SECRET, (err) => {
+    if (err) {
+      return res.status(StatusCodes.UNAUTHORIZED).json({ message: 'Invalid token' });
+    }
 
 
-  //   next();
-  // });
+    next();
+  });
 }
