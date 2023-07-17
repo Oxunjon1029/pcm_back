e7a18f267d7d2a83439fbdf7813f9bd61bec5f5f
Oxunjon1029 <abdusalomov2019oxunjon@gmail.com>
2023-07-18 02:16:11 +0500

    little alteration

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(cookieSession({
   secret: process.env.COOKIE_SECRET,
   cookie: {
     maxAge: 24 * 60 * 60 * 1000,
-    sameSite: 'none',
+    // sameSite: 'none',
     secure: true
   }
 }))
