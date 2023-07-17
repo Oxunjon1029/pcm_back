bf63233c127b2bdddb2f55add132bca2090337e3
Oxunjon1029 <abdusalomov2019oxunjon@gmail.com>
2023-07-17 21:01:58 +0500

    little alteration

--- src/middlewares/ensureAuthenticated.js
+++ src/middlewares/ensureAuthenticated.js
@@ -1,8 +1,8 @@
 const jwt = require('jsonwebtoken');
 const { StatusCodes } = require('http-status-codes')
 module.exports.ensureAuthenticated = (req, res, next) => {
-  console.log("request user",req.user)
-  if (req.user) {
+  console.log("request user",req.isAuthenticated())
+  if (req.isAuthenticated()) {
     next()
   } else {
     const token = req.headers.authorization;
