e6f54ad14bbaed9d5be21f17ec1bea68fb53ec10
Oxunjon1029 <abdusalomov2019oxunjon@gmail.com>
2023-07-17 20:48:47 +0500

    little alteration

--- src/middlewares/ensureAuthenticated.js
+++ src/middlewares/ensureAuthenticated.js
@@ -1,22 +1,25 @@
 const jwt = require('jsonwebtoken');
 const { StatusCodes } = require('http-status-codes')
 module.exports.ensureAuthenticated = (req, res, next) => {
-  if(req.user){
+  console.log(req.user)
+  if (req.user) {
     next()
-  }
-  const token = req.headers.authorization;
+  } else {
+    const token = req.headers.authorization;
 
-  if (!token) {
-    return res.status(StatusCodes.UNAUTHORIZED).json({ message: 'No token provided' });
-  }
-
-  // Verify the token
-  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-    if (err) {
-      return res.status(StatusCodes.UNAUTHORIZED).json({ message: 'Invalid token' });
+    if (!token) {
+      return res.status(StatusCodes.UNAUTHORIZED).json({ message: 'No token provided' });
     }
-    req.user = decoded?.user
 
-    next();
-  });
+    // Verify the token
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      if (err) {
+        return res.status(StatusCodes.UNAUTHORIZED).json({ message: 'Invalid token' });
+      }
+      req.user = decoded?.user
+
+      next();
+    });
+  }
+
 }
